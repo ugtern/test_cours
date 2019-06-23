b7205ab0140a8a2c74b12af53494f4cedefb9c96
sergej <ugtern@gmail.com>
2019-06-23 13:22:58 +0400

    creating tests for users model

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin,\
+                                        BaseUserManager
 
 
 class UserManager(BaseUserManager):
